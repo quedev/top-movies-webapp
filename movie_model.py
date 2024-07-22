9cd9268e579295cde99f0cfe46ddc683b8d6d34e
quantdevlike <hamedabbaszadeh91@gmail.com>
2024-07-22 11:21:52 +0200

    Refactor db and model

---
+++ movie_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from db import db
+
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[int] = mapped_column(String(250))
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer)
+    review: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250))
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
